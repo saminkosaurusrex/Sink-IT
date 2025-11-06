d09d775df36c901dc748f06a82422fa132e33069
saminkosaurusrex <kundratsamuel6@gmail.com>
2025-11-06 16:55:07 +0100

    flutter base

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 Battle ship game
 
 Author: Samuel Kundrat
+
 Description: Final Projct for ITU(User Interface Design) at FUT FIT Brno
---
+++ sink_it/android/app/src/main/kotlin/com/example/sink_it/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sink_it
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
